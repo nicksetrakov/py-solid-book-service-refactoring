6207e797f803ecbb34b8c863d589008a879165a4
Mykola Setrakov <nikset1@gmail.com>
2024-06-19 22:35:02 +0300

    fix blank line and remove else

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from app.displays import DisplayBookConsole, DisplayBookReverse
 from app.serializers import SerializerBookJson, SerializerBookXml
 from app.printers import PrintBookConsole, PrintBookReverse
 
+
 types = {
     "display": {
         "console": DisplayBookConsole.display,
@@ -23,8 +24,8 @@ def main(book: Book, commands: list[tuple[str, str]]) -> None | str:
     for cmd, method_type in commands:
         if cmd in types and method_type in types[cmd]:
             return types[cmd][method_type](book)
-        else:
-            raise ValueError(f"Unknown {cmd} type: {method_type}")
+
+        raise ValueError(f"Unknown {cmd} type: {method_type}")
 
 
 if __name__ == "__main__":
